189e110058dab5831bc39a072ae72961ef72ea1c
Remco <remcopost@gmail.com>
2026-05-28 00:00:27 +0200

    Add license

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Midi Keyboard
 
+[[_TOC_]]
+
+## Introduction
+
+Copyright 2026 Remco Post
+
+[License AGPL](./LICENSE.md)
+
 ## de-mux
 
 The software is designed to use a one-of-eight demux like the 74hc238 to scan a keyboard. With 8 lines this could be up to 5 octave (and a bit).
@@ -32,3 +40,37 @@ Configurable in code, but defined as:
 ## Key state
 
 The (previous) key state is stored in an array. If the value has changed the new value is send over the serial line immediately.
+
+## Keyborad
+
+The keyboard is a simple matrix, 8 input lines, diodes to each key contact and then 8 lines. The current flows from 'Keys in group' to 'Group'.
+
+### Groups
+
+| keys | pin | giga pin |
+|------|-----|----------|
+| 1-8  | 16  | 53       |
+| 9-16 | 1   | 52       |
+| 17-24| 15  | 51       |
+| 25-32| 2   | 50       |
+| 33-40| 14  | 49       |
+| 41-48| 3   | 48       |
+| 49-56| 13  | 47       |
+| 57-61| 4   | 46       |
+
+### Keys in group
+
+| key | pin | giga pin |
+|-----|-----|----------|
+| 1   | 10  | 41       |
+| 2   | 5   | 44       |
+| 3   | 6   | 42       |
+| 4   | 12  | 45       |
+| 5   | 11  | 43       |
+| 6   | 9   | 39       |
+| 7   | 8   | 38       |
+| 8   | 7   | 40       |
+
+### Scanning the keyboard
+
+The 'group' lines are pulled up via the internal pullup resister. If a key is depressed and the 'Keys in group' line is 'LOW', then the 'group' line goes 'LOW'. So, normally all 'Key in group' lines are high. Then one by one each is set to 'LOW', and all 'Group' lines are read to see which of the keys are depressed; those will read 'LOW'.
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
+/*
+
+Copyright (c) 2026 Remco Post
+
+This program is free software: you can redistribute it and/or modify it under the terms of the
+GNU Affero General Public License as published by the Free Software Foundation, either version
+3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with this program.
+If not, see <https://www.gnu.org/licenses/>
+*/
+
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
 #define KEYCOUNT 64
 #define STOPCOUNT 8
 
-#define MIDI_IN PD2
-#define MIDI_OUT PD3
+#define MIDI_IN 8
+#define MIDI_OUT 9
 #define MIDI_BAUDRATE 31250
 
 #define KEY_CHANNEL 1
@@ -14,21 +30,46 @@
 #define KEY_OFF 0x80
 #define KEY_ON 0x90
 
-#define MUX_OUT_0 4
-#define MUX_OUT_1 5
-#define MUX_OUT_2 6
-#define MUX_OUT_0E 7
-#define MUX_OUT_1E 17
+#define DEBOUNCE 63
 
-#define KEY_PINMODE INPUT
-#define KEY0 8
-#define KEY1 9
-#define KEY2 10
-#define KEY3 11
-#define KEY4 12
-#define KEY5 14
-#define KEY6 15
-#define KEY7 16
+#define KEY_PINMODE OUTPUT
+#define GROUPLINE_PINMODE INPUT_PULLUP
+
+#define DEBUG
+
+#define GROUPLINE7 2
+#define KEY0 3
+#define KEY1 4
+#define KEY2 5
+#define KEY3 6
+#define KEY4 7
+
+#define STOPLINE 13
+
+#ifdef DEBUG
+uint8_t keyLines[5] = {KEY0, KEY1, KEY2, KEY3, KEY4};
+uint8_t groupLines[1] = {GROUPLINE7};
+uint8_t stopLines[1] = {STOPLINE};
+
+uint8_t led = 1;
+
+#else
+#define GROUPLINE6 13
+#define GROUPLINE5 14
+#define GROUPLINE4 15
+#define GROUPLINE3 16
+#define GROUPLINE2 17
+#define GROUPLINE1 18
+#define GROUPLINE0 19
+
+#define KEY5 10
+#define KEY6 11
+#define KEY7 12
+
+uint8_t keyLines[8] = {KEY0, KEY1, KEY2, KEY3, KEY4, KEY5, KEY6, KEY7};
+uint8_t groupLines[8] = {GROUPLINE0, GROUPLINE1, GROUPLINE2, GROUPLINE3, GROUPLINE4, GROUPLINE5, GROUPLINE6, GROUPLINE7};
+
+#endif
 
 #define STOP_OFF 0
 #define STOP_ON_0 1
@@ -41,109 +82,136 @@
 
 SoftwareSerial midi(MIDI_IN, MIDI_OUT);
 
-uint8_t senseLine[8] = {KEY0, KEY1, KEY2, KEY3, KEY4, KEY5, KEY6, KEY7};
 int8_t stopShifts[6] = {STOP_OFF, STOP_ON_0, STOP_ON_1, STOP_ON_MIN1, STOP_ON_2, STOP_ON_MIN2};
-uint8_t keyState[KEYCOUNT];
-uint8_t stopState[STOPCOUNT];
+int8_t keyState[KEYCOUNT];
+int8_t stopState[STOPCOUNT];
 int8_t stopShift[STOPCOUNT];
 
-void scanKeyboard ();
-void scanStops ();
+void scanKeyboard();
+void scanStops();
 
-void setupOutputs ();
-void setupInputs();
+void setupKeylines();
+void setupGroupLines();
+void setupStopLines();
+void setupStops(); 
 
 uint8_t calcStop (uint8_t stop, int8_t shift);
 
 void sendMidi (uint8_t channel, uint8_t keyNum, uint8_t val);
 
 void setup() {
-  setupInputs();
-  setupOutputs();
+  setupKeylines();
+  setupGroupLines();
+  setupStopLines();
   midi.begin(MIDI_BAUDRATE);
   Serial.begin(9600);
   Serial.println("Keyboard initialized");
+#ifdef DEBUG
+  Serial.println("Debug mode enabled");
+  setupStops();
+#endif
 }
 
 void loop() {
   scanKeyboard();
   scanStops();
+#ifdef DEBUG
+  led = !led;
+  digitalWrite(LED_BUILTIN, led);
+#endif
 }
 
-void setupInputs () {
-  pinMode(KEY0, KEY_PINMODE);
-  pinMode(KEY1, KEY_PINMODE);
-  pinMode(KEY2, KEY_PINMODE);
-  pinMode(KEY3, KEY_PINMODE);
-  pinMode(KEY4, KEY_PINMODE);
-  pinMode(KEY5, KEY_PINMODE);
-  pinMode(KEY6, KEY_PINMODE);
-  pinMode(KEY7, KEY_PINMODE);
+void setupKeylines () {
+  for (uint8_t i=0; i<sizeof(keyLines); i++) {
+    pinMode(keyLines[i], KEY_PINMODE);
+  }
+  for (uint8_t i=0; i<STOPCOUNT; i++) {
+    stopState[i] = 0;
+    stopShift[i] = 0;
+  }
 }
 
-void setupOutputs () {
-  pinMode(MUX_OUT_0, OUTPUT);
-  pinMode(MUX_OUT_1, OUTPUT);
-  pinMode(MUX_OUT_2, OUTPUT);
-  pinMode(MUX_OUT_0E, OUTPUT);
-  pinMode(MUX_OUT_1E, OUTPUT);
+void setupGroupLines () {
+  for (uint8_t i=0; i<sizeof(groupLines); i++) {
+    pinMode(groupLines[i], GROUPLINE_PINMODE);
+  }
+#ifdef DEBUG
+  pinMode(LED_BUILTIN, OUTPUT);
+#endif
+}
+
+void setupStopLines () {
+  for (uint8_t i=0; i<sizeof(stopLines); i++) {
+    pinMode(stopLines[i], INPUT_PULLUP);
+  }
+}
+
+void setupStops () {
+  sendMidi(STOP_CHANNEL, calcStop(0, stopShift[1]), KEY_ON);
+  sendMidi(STOP_CHANNEL, calcStop(1, stopShift[1]), KEY_ON);
+  sendMidi(STOP_CHANNEL, calcStop(2, stopShift[1]), KEY_ON);
 }
 
 void scanKeyboard () {
-  for (uint8_t bit2 = 0; bit2 < 2; bit2++ ) {
-    digitalWrite(MUX_OUT_2, bit2);
-    for (uint8_t bit1 = 0; bit1 < 2; bit1++ ) {
-      digitalWrite(MUX_OUT_1, bit1);
-      for (uint8_t bit0 = 0; bit0 < 2; bit0++ ) {
-        digitalWrite(MUX_OUT_0, bit0);
-        digitalWrite(MUX_OUT_0E, HIGH);
-        uint8_t base = (bit2 * 4 + bit1 * 2 + bit0) * 8;
-        for (uint8_t line = 0; line < 8; line++) {
-          uint8_t keyNum = base + line;
-          uint8_t val = digitalRead(senseLine[line]);
-          uint8_t old = keyState[keyNum];
-          if (val ^ old) { // if val is not the same as the old state
-            keyState[keyNum] = val;
-            sendMidi(KEY_CHANNEL, keyNum, val);
-          }
-        }
-        digitalWrite(MUX_OUT_0E, LOW);
+  for (uint8_t line = 0; line < sizeof(keyLines); line++) {
+    digitalWrite(keyLines[line], LOW);
+    for (uint8_t i=0; i<sizeof(groupLines); i++) {
+      uint8_t keyNum = (i * 8 + line) + 57;
+      uint8_t val = digitalRead(groupLines[i]); // 0 if the key is pressed, 1 if it's not
+      int8_t old = keyState[keyNum];
+      if (old == 0 && val == 1) { // if the key was not pressed and now is
+        keyState[keyNum] = DEBOUNCE;
+        sendMidi(KEY_CHANNEL, keyNum, KEY_OFF);
+      }
+      if (old == 1 && val == 0) { // if the key was pressed and now is not
+        keyState[keyNum] = -DEBOUNCE;
+        sendMidi(KEY_CHANNEL, keyNum, KEY_ON);
+      }
+      if (old < 0) { // if the key was just released, count up to 0
+        keyState[keyNum]++;
+      }
+      if (old > 1) { // if the key was just pressed, count down to 1
+        keyState[keyNum]--;
       }
     }
+    digitalWrite(keyLines[line], HIGH);
   }
 }
 
 void scanStops () {
-  // for now no MUX, just 
-  digitalWrite(MUX_OUT_1E, HIGH);
-  for (uint8_t line = 0; line < 8; line++) {
-    uint8_t val = digitalRead(senseLine[line]);  
-    uint8_t old = stopState[line];
-    if (val ^ old && val) { // if val is not the same as the old state and it's now on
-      stopState[line] = val;
-      if (stopShift[line] == 5) { // if the stop is at max shift, the stof goes off
-        stopShift[line] = 0;
-        sendMidi(STOP_CHANNEL, calcStop(line, 5), KEY_OFF);
-      } else { // STOP moves to next shift, from OFF to ON_0, to ON_1, etc
-        if (stopShift[line] != 0) {
-          sendMidi(STOP_CHANNEL, calcStop(line, stopShift[line]), KEY_OFF);
+#ifdef DEBUG
+  for (uint8_t line = 0; line < sizeof(keyLines); line++) {
+    digitalWrite(keyLines[line], LOW);
+    for (uint8_t i=0; i<sizeof(stopLines); i++) {
+      uint8_t val = digitalRead(stopLines[i]);
+      uint8_t old = stopState[i];
+      if (val ^ old && val) { // if val is not the same as the old state and it's now on
+        stopState[i] = val;
+        if (stopShift[i] == 5) { // if the stop is at max shift, the stof goes off
+          stopShift[i] = 0;
+          sendMidi(STOP_CHANNEL, calcStop(i, 5), KEY_OFF);
+        } else { // STOP moves to next shift, from OFF to ON_0, to ON_1, etc
+          if (stopShift[i] != 0) {
+            sendMidi(STOP_CHANNEL, calcStop(i, stopShift[i]), KEY_OFF);
+          }
+          stopShift[i]++;
+          sendMidi(STOP_CHANNEL, calcStop(i, stopShift[i]), KEY_ON);
         }
-        stopShift[line]++;
-        sendMidi(STOP_CHANNEL, calcStop(line, stopShift[line]), KEY_ON);
       }
     }
-    digitalWrite(MUX_OUT_1E, LOW);
+    digitalWrite(keyLines[line], HIGH);
   }
+#endif
 }
 
 void sendMidi (uint8_t channel, uint8_t keyNum, uint8_t val) {
-  uint8_t command = (val * 0x10) + channel;
+  uint8_t command = val + channel;
   uint8_t velocity = 0x7F;
   uint8_t key = keyNum + KEY_BASE;
   midi.write(command);
   midi.write(velocity);
   midi.write(key);
-  Serial.println("Sent MIDI message on channel " + String(channel) + " with command " + String(command) + " and key " + String(key));
+  Serial.println("Sent MIDI message on channel " + String(channel) + " with command " + String(val, HEX) + " and key " + String(key, HEX));
 }
 
 uint8_t calcStop (uint8_t stop, int8_t shift) {
