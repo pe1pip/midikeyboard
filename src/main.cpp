a1167706d9ec98d7d76b0519b7bf6f69f0aa0c5a
Remco <remcopost@gmail.com>
2026-06-02 20:32:00 +0200

    due

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ If not, see <https://www.gnu.org/licenses/>
 #define MIDI_IN 8
 #define MIDI_OUT 9
 #define MIDI_BAUDRATE 31250
-#define midi Serial4 // RX3/TX3
+#define midi Serial3
 
 #define KEY_CHANNEL 1
 #define STOP_CHANNEL 0
