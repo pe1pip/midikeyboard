895058592edbf57ef6c83cfed350355324a4c524
Remco <remcopost@gmail.com>
2026-06-03 01:47:35 +0200

    betetr param name

--- lib/midi/midi.h
+++ lib/midi/midi.h
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <generic.h>
 
 // MIDI related constants and variables
 #define MIDI_IN 8
@@ -16,4 +17,4 @@
 
 // MIDI related functions
 uint8_t calcStop (uint8_t stop, int8_t shift);
-void sendMidi (uint8_t channel, uint8_t keyNum, uint8_t val);
+void sendMidi (uint8_t channel, uint8_t keyNum, uint8_t midiCommand);
